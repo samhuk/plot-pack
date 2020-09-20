6b8513d3af281045a492127759ec0d1cfd0a1265
Sam Hughes <samhughes@Sams-MacBook-Pro.local>
2020-09-20 23:38:08 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 Object.defineProperty(exports, "__esModule", { value: true })
 
-exports.Chart = require('./lib/components/chart')
+exports.Chart = require('./lib/components/chart/react')
 
 exports.PieChart = require('./lib/components/pieChart')
